e19c304c73fde2bf36bad5628a67b922d8bc3242
Adam Tilly <tillyjadam@gmail.com>
2021-10-30 13:48:06 -0500

    starting epress server

---
+++ server.js
@@ -0,0 +1,17 @@
+const { json } = require('express');
+const express = require('express')
+const fs = require('fs');
+const path = require('path');
+
+const app = express()
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+const PORT = process.env.PORT || 3001;
+
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
+
+app.listen(PORT, () => {
+  console.log(`API server now on port ${PORT}!`);
+});

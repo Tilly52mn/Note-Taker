b256aaa73c695e7d9e3208ef605d71f112796f72
Adam Tilly <tillyjadam@gmail.com>
2021-10-30 22:22:51 -0500

    it can now get existing notes and started creating how to post data about anew note

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,14 +1,34 @@
 const router = require('express').Router();
 const path = require('path');
-const {notes}= require('../db/db.json')
+const notes = require('../db/db.json')
+
+function createNewNote(body, notes) {
+    const note = body;
+    notes.push(note);
+    fs.writeFileSync(
+        path.join(__dirname, '../db/db.json'),
+        JSON.stringify(note)
+    );
+    return note;
+}
 
 router.get('/notes', (req, res) => {
     const result = notes;
     if (result) {
-      res.json(result);
+        res.json(result);
     } else {
-      res.sendStatus(404);
+        res.sendStatus(404);
     }
-  });
+});
+
+router.post('/notes', (req, res) => {
+    alert("post")
+    // set id based on what the next index of the array will be
+    // req.body.id = animals.length.toString();
+    const note = createNewNote(req.body, notes);
+    res.json(note);
+    console.log(note)
+})
+
 
 module.exports = router

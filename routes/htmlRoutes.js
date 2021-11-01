2e46d17af959426e8209f09148b9f6726c89c04e
Adam Tilly <tillyjadam@gmail.com>
2021-10-31 20:58:29 -0500

    added route to index page and updated json for some more data

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -5,7 +5,9 @@
       res.sendFile(path.join(__dirname,'../public/notes.html'));
   });
 
-
+  router.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname,'../public/index.html'));
+});
   
   module.exports = router;
 
